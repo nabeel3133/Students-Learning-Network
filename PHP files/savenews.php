7515489b2973c369d4a9f9012d4f994803397a94
Nabeel Hussain <31992723+nabeel3133@users.noreply.github.com>
2019-01-24 05:30:52 +0500

    Delete savenews.php

--- PHP files/savenews.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conn=mysqli_connect("localhost","root","","students_network");
-	$title = $_GET['title'];
-	$news = $_GET['news'];
-	$inst_id = $_GET['username'];
-	$sql = "Insert into newsdata (Title,News,Instructor_ID) values('$title','$news','$inst_id')";
-	$conn->query($sql);
-	mysqli_close($conn);
-?>
